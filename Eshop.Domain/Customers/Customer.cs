80fbdca0717e6076f933f173d51130a246259a84
DZietara <39183654+DZietara@users.noreply.github.com>
2024-12-14 12:52:23 +0100

    first commit

---
+++ Eshop.Domain/Customers/Customer.cs
@@ -0,0 +1,28 @@
+﻿using Eshop.Domain.Customers.Events;
+using Eshop.Domain.Customers.Rules;
+using Eshop.Domain.Orders.Events;
+using Eshop.Domain.SeedWork;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Eshop.Domain.Customers;
+
+public class Customer : Entity, IAggregateRoot
+{
+    [BsonRepresentation(BsonType.String)]
+    public string Name { get; private set; }
+
+    public static Customer Create(string name)
+    {
+        CheckRule(new CustomerNameNotEmptyOnlyLettersRule(name));
+
+        return new(name);
+    }
+
+    private Customer(string name) : base(Guid.NewGuid())
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+
+        AddDomainEvent(new CustomerCreatedEvent(Id));
+    }
+}
---
+++ Eshop.Domain/Registry.cs
@@ -0,0 +1,16 @@
+﻿using Eshop.Domain.Customers.Events;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Eshop.Domain;
+
+public static class Registry
+{
+    public static void RegistryDomain(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Registry).Assembly));
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CustomerCreatedEvent>());
+    }
+}
